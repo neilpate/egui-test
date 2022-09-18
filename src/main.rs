8786b277d17543fe6b95b245d593085de6b4700c
Neil <neil@premiergsolutions.com>
2022-09-18 14:43:22 +0200

    feat: Added simple buffered AI

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,62 @@
+use std::thread;
+use std::thread::spawn;
+use std::time;
 use std::time::Instant;
 
+use daqmx::{channels::VoltageChannelBuilder, tasks::Task};
 use eframe::egui;
 use egui::{containers::*, *};
 
+use daqmx::channels::*;
+use daqmx::tasks::*;
+use daqmx::types::*;
+
+use std::sync;
+
 fn main() {
     let options = eframe::NativeOptions::default();
+
+    thread::spawn(|| analogue_input());
+
     eframe::run_native(
         "DAQmx test",
         options,
         Box::new(|_cc| Box::new(App::default())),
     );
+}
 
-    println!("does this happen?");
+fn analogue_input() {
+    let mut task = Task::new("buffered").unwrap();
+    let ch1 = VoltageChannelBuilder::new("Sim-PCIe-6320/ai0").unwrap();
+    task.create_channel(ch1).unwrap();
+    task.configure_sample_clock_timing(
+        None,
+        1000.0,
+        ClockEdge::Rising,
+        SampleMode::ContinuousSamples,
+        10000,
+    )
+    .unwrap();
+
+    let mut buffer = [0.0; 100];
+
+    task.start().unwrap();
+
+    for i in 1..100 {
+        task.read(
+            Timeout::Seconds(1.0),
+            DataFillMode::GroupByChannel,
+            Some(100),
+            &mut buffer[..],
+        )
+        .unwrap();
+
+        for sample in buffer {
+            println!("{}", sample);
+        }
+    }
+
+    drop(task);
 }
 
 struct App {
