81b9cb8fc893c5e1a920af6cb26ab18419cc1a79
Neil <neil@premiergsolutions.com>
2022-09-18 20:58:42 +0200

    feat: Signal is now displayed in GUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+use std::sync::Arc;
 use std::thread;
 use std::thread::spawn;
 use std::time;
@@ -18,17 +20,20 @@ use std::sync::mpsc::{self, Receiver, Sender};
 fn main() {
     let options = eframe::NativeOptions::default();
 
-    let (tx, rx) = mpsc::channel();
+    let (tx_avg, rx_avg) = mpsc::channel();
+    // let (tx_data: Sender<f32>, rx_data: Receiver<f32>) = mpsc::channel();
+    let (tx_data, rx_data) = mpsc::channel::<Vec<f64>>();
 
-    thread::spawn(move || analogue_input(tx));
+    thread::spawn(move || analogue_input(tx_avg, tx_data));
 
     let mut app = App::default();
-    app.receiver = Some(rx);
+    app.avg_receiver = Some(rx_avg);
+    app.data_receiver = Some(rx_data);
 
     eframe::run_native("DAQmx test", options, Box::new(|_cc| Box::new(app)));
 }
 
-fn analogue_input(tx: Sender<f32>) {
+fn analogue_input(tx_avg: Sender<f32>, tx_data: Sender<Vec<f64>>) {
     let mut task = Task::new("buffered").unwrap();
     let ch1 = VoltageChannelBuilder::new("Sim-PCIe-6320/ai0").unwrap();
     task.create_channel(ch1).unwrap();
@@ -54,6 +59,8 @@ fn analogue_input(tx: Sender<f32>) {
         )
         .unwrap();
 
+        tx_data.send(buffer.to_vec());
+
         let mut avg = 0.0;
 
         for sample in buffer {
@@ -61,22 +68,26 @@ fn analogue_input(tx: Sender<f32>) {
         }
         avg = avg / 100.0;
 
-        tx.send(avg as f32);
+        tx_avg.send(avg as f32);
     }
 }
 
 struct App {
-    receiver: Option<Receiver<f32>>,
+    avg_receiver: Option<Receiver<f32>>,
+    data_receiver: Option<Receiver<Vec<f64>>>,
     last_now: Instant,
     avg: f32,
+    data: [f64; 100],
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            receiver: None,
+            avg_receiver: None,
+            data_receiver: None,
             last_now: Instant::now(),
             avg: 0.0,
+            data: [0.0; 100],
         }
     }
 }
@@ -88,7 +99,7 @@ impl eframe::App for App {
         self.last_now = now;
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            let avg = self.receiver.as_ref().unwrap().try_recv();
+            let avg = self.avg_receiver.as_ref().unwrap().try_recv();
             match avg {
                 Ok(data) => self.avg = data,
                 _ => (),
@@ -96,12 +107,20 @@ impl eframe::App for App {
 
             ui.heading(format!("Average: {:.2}", self.avg));
 
-            strings_ui(ui);
+            let data = self.data_receiver.as_ref().unwrap().try_recv();
+            match data {
+                Ok(x) => {
+                    self.data = x.try_into().unwrap();
+                }
+                _ => (),
+            }
+
+            strings_ui(ui, &self.data);
         });
     }
 }
 
-fn strings_ui(ui: &mut Ui) {
+fn strings_ui(ui: &mut Ui, data: &[f64; 100]) {
     let color = if ui.visuals().dark_mode {
         Color32::from_additive_luminance(196)
     } else {
@@ -110,33 +129,28 @@ fn strings_ui(ui: &mut Ui) {
 
     Frame::canvas(ui.style()).show(ui, |ui| {
         ui.ctx().request_repaint();
-        let time = ui.input().time;
 
-        let desired_size = ui.available_width() * vec2(1.0, 0.35);
+        let desired_size = ui.available_width() * vec2(1.0, 1.0);
         let (_id, rect) = ui.allocate_space(desired_size);
 
         let to_screen =
-            emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);
+            emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -5.0..=5.0), rect);
 
         let mut shapes = vec![];
 
-        for &mode in &[2, 3, 5] {
-            let mode = mode as f64;
-            let n = 1200;
-            let speed = 1.5;
+        let n = data.len();
 
-            let points: Vec<Pos2> = (0..=n)
-                .map(|i| {
-                    let t = i as f64 / (n as f64);
-                    let amp = (time * speed * mode).sin() / mode;
-                    let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
-                    to_screen * pos2(t as f32, y as f32)
-                })
-                .collect();
+        let points: Vec<Pos2> = (0..n)
+            .map(|i| {
+                let t = i as f64 / (n as f64);
+                let y = data[i];
+                to_screen * pos2(t as f32, y as f32)
+            })
+            .collect();
 
-            let thickness = 10.0 / mode as f32;
-            shapes.push(epaint::Shape::line(points, Stroke::new(thickness, color)));
-        }
+        let thickness = 1.0;
+        shapes.push(epaint::Shape::line(points, Stroke::new(thickness, color)));
+        //   }
 
         ui.painter().extend(shapes);
     });
